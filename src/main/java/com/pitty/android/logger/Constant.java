48d62b2552768203086ec63f3a1210733608b0d0
Pitty <pitty.xu@gmail.com>
2014-10-20 08:59:15 +0800

    [v1.0] change: add contant.

---
+++ src/main/java/com/pitty/android/logger/Constant.java
@@ -0,0 +1,10 @@
+package com.pitty.android.logger;
+
+/**
+ * Created by Pitty on 14/10/20.
+ */
+public class Constant {
+    // static log switcher
+    public static final boolean LOG = true;
+
+}
---
+++ src/main/java/com/pitty/android/logger/LEVEL.java
@@ -0,0 +1,14 @@
+package com.pitty.android.logger;
+
+/**
+ * Created by Pitty on 14/10/20.
+ */
+public enum  LEVEL {
+    O, //OFF
+    V, // Verbose
+    D, // Debug
+    I, // Info
+    W, // Warn
+    E, // Error
+    A  // Assert
+}
